91edd031fdc51f5271736b3449f767272eb19a75
Programistich <me@programistich.com>
2024-02-02 17:52:49 +0200

    Initial Commit

---
+++ DzhosLab1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DzhosLab1
+//
+//  Created by Programistich on 02.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
